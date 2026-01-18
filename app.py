127dca9d004eda8b8f5e5570c16e191244964091
silvareis78 <silvareis78@gmail.com>
2026-01-17 23:44:31 -0300

    Update app.py

--- app.py
+++ app.py
@@ -665,14 +665,14 @@ if selecionado == "Painel Inicial":
     with col_per:
         with st.container(height=160, border=True):
             # Título do quadro um pouco mais para baixo como pediu antes
-            st.markdown("<div style='margin-top: -10px; margin-bottom: 5px; font-size: 0.9rem;'>🔍 <b>Período</b></div>", unsafe_allow_html=True)
+            st.markdown("<div style='margin-top: 5px; margin-bottom: 5px; font-size: 0.9rem;'>🔍 <b>Período</b></div>", unsafe_allow_html=True)
             
             # BLOCO MÊS
             st.markdown("<div style='font-size: 0.75rem;'><b>Selecione o Mês:</b></div>", unsafe_allow_html=True)
             mes_sel = st.selectbox("Mês", ["JANEIRO", "FEVEREIRO", "MARÇO", "ABRIL", "MAIO", "JUNHO", "JULHO", "AGOSTO", "SETEMBRO", "OUTUBRO", "NOVEMBRO", "DEZEMBRO"], index=0, label_visibility="collapsed")
             
             # --- AJUSTE AQUI: Diminuí o espaço para juntar o Ano ao Mês ---
-            st.markdown("<div style='margin-top: -15px; font-size: 0.75rem;'><b>Selecione o Ano:</b></div>", unsafe_allow_html=True)
+            st.markdown("<div style='margin-top: 5px; font-size: 0.75rem;'><b>Selecione o Ano:</b></div>", unsafe_allow_html=True)
             ano_sel = st.selectbox("Ano", ["2024", "2025", "2026"], index=2, label_visibility="collapsed")
 
     with col_des:
@@ -1069,6 +1069,7 @@ if selecionado == "Cartões":
 
     except Exception as e:
         st.error(f"Erro ao carregar a tela: {e}")
+
 
 
 
